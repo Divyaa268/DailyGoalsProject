caeb093c25416a2244a2be8230ad2ad00c97fa13
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-05-17 02:07:29 +0530

    Added JS code.
    Added JS code.

---
+++ index.js
@@ -0,0 +1,4 @@
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const form = document.querySelector("form");
+const container = document.querySelectorAll(".container");
