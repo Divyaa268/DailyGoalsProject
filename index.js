5ec8135c5180e11479c50d7e9b5850dae07a9bf0
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-05-17 17:56:22 +0530

    ADDED NEW FUNCTIONS
    ADDED NEW FUNCTIONS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const title = document.getElementById("title");
 const description = document.getElementById("description");
 const form = document.querySelector("form");
-const container = document.querySelectorAll(".container");
+const container = document.querySelector(".container");
 
 const tasks = [];
 
@@ -14,17 +14,52 @@ function showAllTasks()
 
         const innerDiv = document.createElement("div");
         div.append(innerDiv);
+
+        const p = document.createElement("p");
+        p.innerText = value.title;
+        innerDiv.append(p);
+
+        const span = document.createElement("span");
+        span.innerText = value.description;
+        innerDiv.append(span);
+
+        const btn = document.createElement("button");
+        btn.setAttribute("class", "delBtn");
+
+    btn.innerText = "-";
+
+    
+    div.append(btn);
+
+    container.append(div);
+
+
     })
+}
 
 
+function removeTasks()
+{
+    tasks.forEach(() =>
+    {
+        const div = document.querySelector(".task");
+        div.remove();
+
+    })
 }
 
 form.addEventListener("submit", (e) => 
 {
     e.preventDefault(); // to prevent reloading page
+
+    removeTasks(); // remove tasks before pushing new ones
+
     tasks.push({
         title: title.value,
         description: description.value,
         
-    })
-})
+    });
+    // console.log(tasks);
+
+    showAllTasks();
+});
