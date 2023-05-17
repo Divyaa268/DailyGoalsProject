fa1aca34619b0798cb75cc7c20898a3c06c96d51
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-05-17 17:25:20 +0530

    Updated index.js
    Updated index.js

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ const title = document.getElementById("title");
 const description = document.getElementById("description");
 const form = document.querySelector("form");
 const container = document.querySelectorAll(".container");
+
+const tasks = [];
+
+function showAllTasks()
+{
+    tasks.forEach((value, index) => 
+    {
+        const div = document.createElement("div");
+        div.setAttribute("class", "task");
+
+        const innerDiv = document.createElement("div");
+        div.append(innerDiv);
+    })
+
+
+}
+
+form.addEventListener("submit", (e) => 
+{
+    e.preventDefault(); // to prevent reloading page
+    tasks.push({
+        title: title.value,
+        description: description.value,
+        
+    })
+})
